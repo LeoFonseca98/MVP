14694751ab8c8cfca12a3ee6b9d71bc7092defb7
LeoFonseca89 <leo-fonseca@outlook.com>
2026-04-08 18:36:34 -0300

    nova atualização3

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,12 @@ import transacaoRoutes from "./routes/transacao.routes";
 
 const app = express();
 
+// 🟢 Habilita CORS para o frontend
+app.use(cors({
+  origin: "https://mvp-front-47ap-git-main-leonardo-fonsecas-projects.vercel.app"
+}));
+
+
 app.use(cors());
 app.use(express.json());
 
